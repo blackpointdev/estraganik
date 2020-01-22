8ddc091228c48446d799c3f4d6df4ee0906b8ce4
blackpointdev <marcinw1998@gmail.com>
2020-01-22 23:59:57 +0100

    OfferController small fixes.

--- src/Controllers/OfferController.php
+++ src/Controllers/OfferController.php
@@ -4,9 +4,20 @@
 namespace Src\Controllers;
 
 
+use Src\Repositories\UserRepository;
+use Src\System\DBConnection;
+
 class OfferController extends Controller
 {
     public function createOffer() {
+        if($this->isPost()) {
+            $dbConnection = (new DBConnection())->connect();
+            $userRepository = new UserRepository($dbConnection);
+
+            $title = !empty($_POST['name']) ? trim($_POST['name']) : null;
+            $surname = !empty($_POST['surname']) ? trim($_POST['surname']) : null;
+            $address = !empty($_POST['address']) ? trim($_POST['address']) : null;
+        }
         $this->render('create_offer');
     }
 }
